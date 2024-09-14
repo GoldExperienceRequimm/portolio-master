d6a8ec3fa2d3a4da547d02da302d4994c762989b
Eshan <aansaf99@gmail.com>
2024-09-14 18:36:14 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { useState } from "react";
-import {darkTheme, lightTheme} from "./Utils/themes.js"
+
 import Navbar from "./Components/Nav/index.jsx";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -12,6 +12,29 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import ProjectDetails from "./components/ProjectDetails";
 import styled from "styled-components";
+
+const darkTheme = {
+  bg: "#1C1C27",
+  bgLight: "#1C1E27",
+  primary: "#854CE6",
+  text_primary: "#F2F3F4",
+  text_secondary: "#b1b2b3",
+  card: "#171721",
+  card_light: "#191924",
+  button: "#854CE6",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary: "#be1adb",
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFFFFF",
+  button: "#5c5b5b",
+};
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
