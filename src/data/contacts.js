d54e98b11b7bc124b1da8025b0b1389727086924
Eshan <aansaf99@gmail.com>
2024-10-21 18:00:44 +0530

    sec

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -1,5 +1,5 @@
 export const Bio = {
-  name: "Eshan",
+  name: "Es",
   roles: ["Full Stack Developer", "UI/UX Designer", "Game Developer"],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
