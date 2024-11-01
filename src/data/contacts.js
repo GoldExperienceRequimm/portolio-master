17c5dabb17dba2a0ce000525d823a3558c0e258a
Eshan <aansaf99@gmail.com>
2024-11-01 17:54:48 +0530

    f

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -63,7 +63,8 @@ export const skills = [
       },
       {
         name: "Express Js",
-        image: "../../public/express-svgrepo-com.svg",
+        image:
+          "https://w7.pngwing.com/pngs/846/87/png-transparent-mean-solution-stack-express-js-node-js-javascript-github-text-trademark-logo-thumbnail.png",
       },
       {
         name: "MySQL",
